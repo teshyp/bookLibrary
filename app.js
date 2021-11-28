b762a208797e579f470e48b19b159116b30907ea
Tesh Patel <tesh.p@hotmail.co.uk>
2021-11-28 00:29:14 +0000

    Feature added: add new book

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //Get Elements
 const libList = document.getElementById("library-list");
 const firstCard = document.getElementById("first-card");
+const addBtn = document.getElementById("add");
 
-// Event listeners
 
 // Library object
 
@@ -33,7 +33,43 @@ function Book(title, author, pages, read) {
 // Function to create book using input data
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook)
+    myLibrary.push(newBook);
+    printSingleBook()
+    console.log(newBook)
+}
+
+function printSingleBook() {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("individual-card");
+    libList.appendChild(cardDiv);
+
+    let newTitleP = document.createElement("p");
+    newTitleP.innerText = myLibrary[i].title;
+    cardDiv.appendChild(newTitleP)
+
+    let newAuthorP = document.createElement("p");
+    newAuthorP.innerText = myLibrary[i].author;
+    cardDiv.appendChild(newAuthorP)
+
+    let pagesNo = document.createElement("p");
+    pagesNo.innerText = myLibrary[i].pages;
+    cardDiv.appendChild(pagesNo)
+
+    let btnSpan = document.createElement("span");
+    cardDiv.appendChild(btnSpan)
+
+    let readBtn = document.createElement("button");
+    readBtn.innerHTML = myLibrary[i].read;
+    readBtn.classList.add("read-btn");
+    readBtn.value = "not-read"
+    btnSpan.appendChild(readBtn);
+
+    let newDelBtn = document.createElement("button");
+    newDelBtn.innerHTML = "Delete";
+    newDelBtn.classList.add("delete-btn")
+    btnSpan.appendChild(newDelBtn);
+
+    console.log(myLibrary[i]);
 }
 
 // Function to display books in library
@@ -77,7 +113,16 @@ function printBooks() {
 
 printBooks()
 
-// Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
+//Adding new books to library
+addBtn.addEventListener("click", function () {
+    const bookTitle = document.getElementById("bookTitle").value;
+    const bookAuthor = document.getElementById("bookAuthor").value;
+    const bookPages = document.getElementById("bookPages").value;
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, "Not Read");
+
+})
+
+
 // Add a button on each book’s display to remove the book from the library.
 // You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
 // Add a button on each book’s display to change its read status. - To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
